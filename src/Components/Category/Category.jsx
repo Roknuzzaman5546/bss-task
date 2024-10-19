980431957c62041863355dfb4bec8e19ea3a4bc8
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-10-20 02:57:08 +0600

    carusol code added

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -4,11 +4,22 @@ import categoryImg3 from '../../assets/category/HEMA-2024-categoryslider-280x280
 import categoryImg4 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk3738-dameskleding.webp';
 import categoryImg5 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk3738-dameskleding.webp';
 import categoryImg6 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk3738-dameskleding.webp';
+import { Virtual, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
 
 const Category = () => {
+
     return (
         <div className='md:max-w-screen-2xl w-11/12 mx-auto mt-2.5 px-1 overflow-hidden'>
             <div className=' flex justify-center items-center gap-2'>
+                {/* img 1 */}
                 <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
                     <img
                         src={categoryImg1}
@@ -16,6 +27,7 @@ const Category = () => {
                         alt=""
                     />
                 </div>
+                {/* img 2 */}
                 <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
                     <img
                         src={categoryImg2}
@@ -23,6 +35,7 @@ const Category = () => {
                         alt=""
                     />
                 </div>
+                {/* img 3 */}
                 <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
                     <img
                         src={categoryImg3}
@@ -30,6 +43,7 @@ const Category = () => {
                         alt=""
                     />
                 </div>
+                {/* img 4 */}
                 <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
                     <img
                         src={categoryImg4}
@@ -37,6 +51,7 @@ const Category = () => {
                         alt=""
                     />
                 </div>
+                {/* img 5 */}
                 <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
                     <img
                         src={categoryImg5}
@@ -44,6 +59,7 @@ const Category = () => {
                         alt=""
                     />
                 </div>
+                {/* img 6 */}
                 <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
                     <img
                         src={categoryImg6}
@@ -52,6 +68,81 @@ const Category = () => {
                     />
                 </div>
             </div>
+            <Swiper
+                modules={[Virtual, Navigation, Pagination]}
+                slidesPerView={6}
+                centeredSlides={true}
+                spaceBetween={10}
+                pagination={{
+                    type: 'custom',
+                }}
+                navigation={true}
+                virtual
+            >
+
+                <SwiperSlide>
+                    <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
+                        <img
+                            src={categoryImg1}
+                            className='rounded-full transition-transform duration-500 ease-in-out hover:scale-110 hover:shadow-lg'
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
+                        <img
+                            src={categoryImg2}
+                            className='rounded-full transition-transform duration-500 ease-in-out hover:scale-110 hover:shadow-lg'
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
+                        <img
+                            src={categoryImg3}
+                            className='rounded-full transition-transform duration-500 ease-in-out hover:scale-110 hover:shadow-lg'
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
+                        <img
+                            src={categoryImg4}
+                            className='rounded-full transition-transform duration-500 ease-in-out hover:scale-110 hover:shadow-lg'
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
+                        <img
+                            src={categoryImg5}
+                            className='rounded-full transition-transform duration-500 ease-in-out hover:scale-110 hover:shadow-lg'
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="overflow-hidden rounded-full h-24 w-24 cursor-pointer">
+                        <img
+                            src={categoryImg6}
+                            className='rounded-full transition-transform duration-500 ease-in-out hover:scale-110 hover:shadow-lg'
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+
+
         </div>
     );
 };
