cd0d262966ff86559774e57dcc378b1816de034a
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-10-31 01:16:23 +0600

    add banner image project picture

--- src/Components/CardSlide/CardSlide.jsx
+++ src/Components/CardSlide/CardSlide.jsx
@@ -1,4 +1,4 @@
-import cardSlideImg from '../../assets/cardSlide/cardSlideBanner.png'
+import cardSlideImg from '../../assets/cardSlide/HEMADD_Header_F5F5F5.svg'
 import cardcarosulImg from '../../assets/cardSlide/HEMA-2024-homepage-carrousel-600x750-wk41-42-beddengoed.webp'
 import cardcarosulImg1 from '../../assets/cardSlide/HEMA-2024-homepage-carrousel-600x750-wk41-42-bedtextiel 1.webp'
 import cardcarosulImg2 from '../../assets/cardSlide/HEMA-2024-homepage-carrousel-600x750-wk41-42-bloempottenvaasjes.webp'
@@ -13,21 +13,21 @@ import "react-multi-carousel/lib/styles.css";
 const CardSlide = () => {
     return (
         <div>
-            <div className='max-w-[1350px] bg-[#ce8dff]  rounded-3xl mx-auto md:mt-24 mt-32'>
-                <div className=" md:max-w-screen-2xl w-11/12 flex lg:flex-row md:flex-col-reverse flex-col-reverse justify-center xl:py-36 lg:py-28 md:py-24 py-24 mx-auto">
-                    <div className=' lg:w-[40%] md:full w-full md:-mt-16 mt-1 lg:pt-0 md:pt-16 pt-0 md:pb-4 pb-0'>
-                        <h2 className=' font-sans xl:text-[33px] lg:text-[33px] md:text-[28px] text-[33px] font-bold text-[#323232] xl:w-[65%] lg:w-[80%] md:w-[60%] w-full text-justify'>alles om van je huis seen vrolijik thuis te maken</h2>
-                        <p className=' text-xs my-4 text-[#323232]'>van klurjki kaarsen lot beddengoed om  ub weg te <br /> dromen</p>
-                        <button className=' border-2 border-[#323232] text-[#323232] hover:text-[#fff] hover:bg-[#323232] font-semibold py-3 px-5 rounded-lg'>wontedek wonen & slapen</button>
+            <div className='max-w-[1350px] bg-[#f5f5f5]  rounded-md mx-auto md:mt-7 mt-20 pt-[38px] pb-32'>
+                <div className=" md:max-w-screen-2xl w-11/12 flex lg:flex-row md:flex-col-reverse flex-col-reverse justify-between items-center mx-auto">
+                    <div className=' lg:w-[47%] md:full w-full lg:pt-0 md:pt-16 pt-0 '>
+                        <h2 className=' xl:text-[33px] lg:text-[30px] md:text-[28px] text-[33px] font-extrabold text-[#323232] xl:w-[300px] lg:w-[300px] md:w-[60%] w-full text-justify'>profiteer nu van te gekke acties</h2>
+                        <p className=' my-4 text-[#323232] font-hurme'>en scoor je favorieten met dolle doordraai prijzen</p>
+                        <button className=' mt-2 border-2 border-[#323232] text-[#323232] hover:text-[#fff] hover:bg-[#323232] font-bold font-hurme text-lg py-3 px-5 rounded-lg w-[63%]'>shop nu</button>
                     </div>
-                    <div className=' xl:-mt-48 lg:-mt-36 md:-mt-32 -mt-48 xl:w-[60%] lg:w-[60%] md:w-full w-full'>
-                        <img className=' rounded-lg' src={cardSlideImg} alt="" />
+                    <div className='xl:w-[60%] lg:w-[53%] md:w-full w-full'>
+                        <img className=' rounded-lg object-cover' src={cardSlideImg} alt="" />
                     </div>
                 </div>
             </div>
             {/* carosul part */}
             <div className=''>
-                <div className=' md:-mt-24 -mt-20 md:max-w-[1280px] custom-range:px-9 md:px-[10px] mx-auto'>
+                <div className=' md:-mt-20 -mt-116 md:max-w-[1280px] custom-range:px-9 md:px-[10px] mx-auto'>
                     <Carousel
                         additionalTransfrom={0}
                         arrows
