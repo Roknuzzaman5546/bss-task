a6f51442907cd3326946a91c280b3d7e3bd90ab9
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-11-08 01:00:00 +0600

    category navbar code added

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,3 +1,19 @@
+// import categoryImg1 from '../../assets/category/download.jpeg';
+import categoryImg2 from '../assets/category/HEMA-2024-inspiratie-800x800-wk3738-jassen2.webp';
+// import categoryImg3 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk3940-jongenmeisje.webp';
+// import categoryImg4 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk3738-dameskleding.webp';
+// import categoryImg5 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk3940-kleding.webp';
+// import categoryImg6 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk41-42-dbo-nieuw.webp';
+// import categoryImg7 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk1718-kaletaart.webp';
+// import categoryImg8 from '../../assets/category/HEMA-2023-categoryslider-280x280-wk22-vrije-tijd-1.webp';
+// import categoryImg9 from '../../assets/category/HEMA-2024-categoryslider-280x280-keuken.webp';
+// import categoryImg10 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk37-cadeaus-feest.webp';
+// import categoryImg11 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk09-makeup.webp';
+// import categoryImg12 from '../../assets/category/HEMA-2024-categoryslider-280x280-wk1718-educatiefspeelgoed.webp';
+// import categoryImg13 from '../../assets/category/HEMA-2023-categoryslider-280x280-wk24-foto.webp';
+// import categoryImg14 from '../../assets/category/HEMA-2024-categoryslider-280x280-vriendenprijsjes.webp';
+// import categoryImg15 from '../../assets/category/HEMA-categoryslider-2023-280x280-aanbiedingen.webp';
+// import categoryImg16 from '../../assets/category/HEMA-categoryslider-2023-280x280-nieuw.webp';
 import { FaRegUser } from 'react-icons/fa';
 import logo from '../assets/logo/logo.svg'
 import love from '../assets/icon/love.png'
@@ -46,12 +62,67 @@ const Navbar = () => {
                     </div>
                 </div>
                 {/* down menu */}
-                <div className={`flex ${isNavbarJumping ? 'xl:py-[16px]' : 'xl:py-[15px]'}  lg:py-[16px] md:py-[15px] py-[15px] justify-between items-center custom-range:px-12 px-5  max-w-[1280px] mx-auto`}>
+                <div className="w-full ">
+                    <div className={`flex ${isNavbarJumping ? 'xl:py-[16px]' : 'xl:py-[15px]'}  justify-between items-center custom-range:px-12 px-5 max-w-[1280px] mx-auto `}>
+                        <div className='flex items-center '>
+                            <img className='mr-[102px]' src={logo} height={56} width={56} alt="" />
+                        </div>
+                        <div className='category group cursor-pointer'>
+                            <div className={`lg:flex md:hidden hidden justify-between items-center gap-2 mr-[33px] overflow-hidden z-10 h-[88px]`}>
+                                <HiOutlineBars3 className='text-[25px] font-bold text-[#292929]' />
+                                <h2 className='font-semibold text-sm text-[#323232]'>categorieën</h2>
+                                <IoIosArrowDown className='text-[#323232] transition-transform duration-500 group-hover:rotate-180' />
+                            </div>
+                            <div className=" category-info bottom-0 left-0 top-[88px] w-full bg-white transition-transform duration-700 transform scale-y-0 group-hover:scale-y-90 origin-top h-[450px] absolute">
+                                <div className="max-w-[1280px] mx-auto px-12 ">
+                                    <div className='border-t-2 flex justify-between items-start'>
+                                        <div className=' border-r-2 w-[33.33%]'>
+                                            <h2 className='font-bold font-hurme leading-5 pt-5'>categorieën</h2>
+                                            <div className=' flex justify-between items-center gap-2'>
+                                                <img src={categoryImg2} width={36} height={39} className=' rounded-full' alt="" />
+                                                <p className=' border-b pt-2'>sinterklass</p>
+                                            </div>
+                                            <div className=' flex justify-between items-center gap-2 mt-5'>
+                                                <img src={categoryImg2} width={36} height={39} className=' rounded-full' alt="" />
+                                                <p>Karest</p>
+                                            </div>
+                                        </div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center items-center w-full mr-[55px]'>
+                            <input type="text" placeholder='Waar be je naar op Zoak?' className='rounded-3xl h-[44px] w-full outline-0 ring-0 bg-[#efefef] hover:bg-[#dfdfdf] py-3 px-6 placeholder:text-[#767676] placeholder:lowercase placeholder:font-hurme font-hurme placeholder:text-[16px] placeholder:leading-5 placeholder:font-normal transition-colors duration-700' />
+                            <LuSearch className='-ml-11 text-[21px] font-semibold' />
+                        </div>
+
+                        <div className='flex items-center gap-3 mr-2'>
+                            <button className='flex justify-center items-center gap-2 text-[18px] hover:bg-[#efefef] px-3 py-2 rounded-lg transition-colors duration-700'>
+                                <FaRegUser className='text-[#323232]' />
+                                <p className='text-sm font-semibold text-[#323232]'>inloggen</p>
+                            </button>
+                            <button className='hover:bg-[#efefef] px-2 py-2 h-9 w-9 rounded-lg transition-colors duration-500'>
+                                <img src={love} alt="" />
+                            </button>
+                            <button className='hover:bg-[#efefef] px-2 py-2.5 rounded-lg transition-colors duration-500'>
+                                <BsHandbag className='text-[20px] text-[#323232]' />
+                            </button>
+                        </div>
+                        <div className='lg:hidden md:flex flex'>
+                            <HiOutlineBars3 className='text-[25px] font-bold text-[#323232]' />
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div className={`flex ${isNavbarJumping ? 'xl:py-[16px]' : 'xl:py-[15px]'}  lg:py-[16px] md:py-[15px] py-[15px] justify-between items-center custom-range:px-12 px-5  max-w-[1280px] mx-auto`}>
                     <div className='flex items-center '>
                         <img className='mr-[102px] ' src={logo} height={56} width={56} alt="" />
                     </div>
-                    <div className='category group cursor-pointer'>
-                        <div className={`lg:flex md:hidden hidden justify-between items-center gap-2 mr-[33px]`}>
+                    <div className='category group cursor-pointer relative'>
+                        <div className={`lg:flex md:hidden hidden justify-between items-center gap-2 mr-[33px] overflow-hidden`}>
                             <HiOutlineBars3 className='text-[25px] font-bold text-[#292929]' />
                             <h2 className='font-semibold text-sm text-[#323232]'>categorieën</h2>
                             <IoIosArrowDown className='text-[#323232] transition-transform duration-500 group-hover:rotate-180' />
@@ -81,7 +152,7 @@ const Navbar = () => {
                     <div className='lg:hidden md:flex flex'>
                         <HiOutlineBars3 className=' text-[25px] font-bold text-[#323232]' />
                     </div>
-                </div>
+                </div> */}
             </div>
             {/* for mobile device */}
             <div className='md:hidden block'>
