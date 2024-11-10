96880ba610abfad3847f1d62c1c27f325f479f20
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-11-10 15:14:59 +0600

    navbar mobile search code added

--- src/Shared/NavMobile.jsx
+++ src/Shared/NavMobile.jsx
@@ -3,6 +3,7 @@ import { FaHeadset, FaRegUser } from 'react-icons/fa';
 import { FiCamera, FiSearch } from 'react-icons/fi';
 import { HiOutlineBars3 } from 'react-icons/hi2';
 import { LuSearch, LuTicket } from 'react-icons/lu';
+import { PiArrowLeftBold } from "react-icons/pi";
 import './Navbar.css'
 import { useEffect, useState } from 'react';
 import love from '../assets/icon/love.png'
@@ -14,7 +15,7 @@ import { IoLocationOutline } from 'react-icons/io5';
 
 const NavMobile = ({ isNavbarJumping, logo }) => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(true);
-    // const [isSearchClickMob, setIsSearchClickMob] = useState(false);
+    const [isSearchClickMob, setIsSearchClickMob] = useState(false);
     const [categoryData, setCategoryData] = useState([]);
 
     useEffect(() => {
@@ -145,28 +146,27 @@ const NavMobile = ({ isNavbarJumping, logo }) => {
                 </div>
             </div>
             <div
-                // onClick={() => setIsSearchClickMob(!isSearchClickMob)}
-                className={`${isDrawerOpen ? ' relative w-11/12 mx-auto' : 'hidden'} toggle-button`}>
+                onClick={() => setIsSearchClickMob(!isSearchClickMob)}
+                className={`${isDrawerOpen ? ' relative w-11/12 mx-auto' : 'hidden'}`}>
                 <input type="text" name="" id="" placeholder='Waar be je naar op Zoak?' className=' rounded-3xl h-[44px] w-full outline-0 ring-0 bg-[#efefef] hover:bg-[#dfdfdf] py-3 px-6 placeholder:text-[#767676] placeholder:text-sm transition-colors duration-700' />
                 <LuSearch className='text-xl absolute top-[29%] right-4' />
             </div>
 
 
             {/* Mobile Searchbar box */}
-            {/* <div className={`side_bar  ${isSearchClickMob ? '-mt-[8px]' : '-mt-[700px]'}
-                     w-full bg-white transition-all duration-500 ease-in-out z-[310] pt-[18.5px] fixed h-full`}>
+            <div className={` ${isSearchClickMob ? '-mt-[110px]' : '-mt-[700px]'}
+                     w-full bg-white transition-all duration-500 ease-in-out z-[310] pt-[5px] fixed h-full`}>
                 <div className=' max-w-[1280px] mx-auto custom-range:px-12 px-5'>
-                    <div className=' flex justify-between items-center'>
-                        <div className='flex justify-center items-center w-[45%] mx-auto'>
-                            <input type="text" placeholder='Waar be je naar op Zoak?' className='rounded-3xl h-[44px] w-full outline-0 ring-0 bg-[#efefef] hover:bg-[#dfdfdf] py-3 px-6 placeholder:text-[#1e0b0b] placeholder:lowercase placeholder:font-hurme font-hurme placeholder:text-[16px] placeholder:leading-5 placeholder:font-normal transition-colors duration-700' />
-                            <LuSearch className='-ml-11 text-[21px] font-semibold' />
-                        </div>
-                        <div className=' flex justify-center items-center gap-3 hover:bg-[#efefef] px-3 py-2 rounded-lg transition-colors duration-700 cursor-pointer' onClick={() => setIsSearchClickMob(!isSearchClickMob)}>
-                            <p className=' font-hurme font-semibold'>annuleren</p>
-                            <FaXmark className=' text-xl'></FaXmark>
+                    <div className=' flex justify-between items-center py-3'>
+                        <div className='flex justify-center items-center w-full mx-auto bg-[#efefef] px-4 rounded-3xl'>
+                            <div className='cursor-pointer' onClick={() => setIsSearchClickMob(!isSearchClickMob)}>
+                                <PiArrowLeftBold className=' text-xl'></PiArrowLeftBold>
+                            </div>
+                            <input type="text" className='h-[44px] w-full outline-0 ring-0 bg-[#efefef] transition-colors duration-700' />
+                            <LuSearch className='text-[21px] font-semibold' />
                         </div>
                     </div>
-                    <div className=' w-2/5 mx-auto mt-12'>
+                    <div className=' mt-5'>
                         <h2 className=' text-lg font-bold font-hurme text-black pb-4'>populair</h2>
                         <div className=' flex justify-start gap-3 hover:bg-[#efefef] rounded-md transition-colors duration-700 cursor-pointer py-2.5 pl-2'>
                             <LuSearch className='text-[21px] font-semibold' />
@@ -182,7 +182,7 @@ const NavMobile = ({ isNavbarJumping, logo }) => {
                         </div>
                     </div>
                 </div>
-            </div>  */}
+            </div>
         </div>
     );
 };
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -54,23 +54,25 @@ const Navbar = () => {
                 {/* for desktop & tab device */}
                 <div className={`md:block hidden relative`}>
                     {/* Upper menu */}
-                    <div className={`max-w-[1280px] mx-auto custom-range:px-12 px-5 pt-[12px] ${isNavbarJumping ? "h-0 opacity-0 hidden transition-all duration-300 ease-in-out" : "h-auto opacity-100"} ${isNavCategoryHovered ? " bg-[#8b8b8b] opacity-90" : "opacity-100"}`}>
-                        <div className={`${isNavCategoryHovered ? 'border-0' : ' border-b'} border-[#f5f5f5] lg:flex md:hidden hidden justify-between items-center pb-[10px]`}>
-                            <div className="flex items-center">
-                                <a className="navAfter relative font-semibold mr-6 text-[#323232] text-[13px] font-sans cursor-pointer">fotoservice</a>
-                                <a className="navAfter relative font-semibold mr-6 text-[#323232] text-[13px] font-sans cursor-pointer">tickets & deals</a>
-                                <a className="navAfter relative font-semibold mr-6 text-[#323232] text-[13px] font-sans cursor-pointer">verzekeringen</a>
-                                <a className="navAfter relative font-semibold text-[#323232] text-[13px] font-sans cursor-pointer">inspiratie</a>
-                            </div>
-                            <div className="flex items-center">
-                                <a className="navAfter relative mr-6 font-semibold text-[#323232] text-[13px] font-sans cursor-pointer">winkels</a>
-                                <a className="navAfter relative mr-6 font-semibold text-[#323232] text-[13px] font-sans  cursor-pointer">oklantenpas</a>
-                                <a className="navAfter relative font-semibold text-[#323232] text-[13px] font-sans cursor-pointer">klantenservice</a>
+                    <div className={`${isNavCategoryHovered ? " bg-[#8b8b8b] opacity-90" : "opacity-100"}`}>
+                        <div className={`max-w-[1280px] mx-auto custom-range:px-12 px-5 pt-[12px] ${isNavbarJumping ? "h-0 opacity-0 hidden transition-all duration-300 ease-in-out" : "h-auto opacity-100"}`}>
+                            <div className={`${isNavCategoryHovered ? 'border-0' : ' border-b'} border-[#f5f5f5] lg:flex md:hidden hidden justify-between items-center pb-[10px]`}>
+                                <div className="flex items-center">
+                                    <a className="navAfter relative font-semibold mr-6 text-[#323232] text-[13px] font-sans cursor-pointer">fotoservice</a>
+                                    <a className="navAfter relative font-semibold mr-6 text-[#323232] text-[13px] font-sans cursor-pointer">tickets & deals</a>
+                                    <a className="navAfter relative font-semibold mr-6 text-[#323232] text-[13px] font-sans cursor-pointer">verzekeringen</a>
+                                    <a className="navAfter relative font-semibold text-[#323232] text-[13px] font-sans cursor-pointer">inspiratie</a>
+                                </div>
+                                <div className="flex items-center">
+                                    <a className="navAfter relative mr-6 font-semibold text-[#323232] text-[13px] font-sans cursor-pointer">winkels</a>
+                                    <a className="navAfter relative mr-6 font-semibold text-[#323232] text-[13px] font-sans  cursor-pointer">oklantenpas</a>
+                                    <a className="navAfter relative font-semibold text-[#323232] text-[13px] font-sans cursor-pointer">klantenservice</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                     {/* down menu */}
-                    <div className="w-full ">
+                    <div className="w-full bg-white">
                         <div className={`flex ${isNavbarJumping ? 'xl:py-[16px]' : 'xl:py-[15px]'}  justify-between items-center custom-range:px-12 px-5 max-w-[1280px] mx-auto `}>
                             <div className='flex items-center '>
                                 <img className='mr-[102px]' src={logo} height={56} width={56} alt="" />
@@ -113,7 +115,7 @@ const Navbar = () => {
                     <NavMobile
                         isNavbarJumping={isNavbarJumping}
                         logo={logo} isSearchClick={isSearchClick}
-                        />
+                    />
                 </div>
             </div>
             {/* SearchBar import from NavSearchBar */}
